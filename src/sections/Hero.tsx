661e76005a5028d85b1e0c26d7a84c72066b38de
Hritul <hritul.srivastava@gmail.com>
2024-09-20 15:17:38 +0530

    product show case section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,9 +9,7 @@ export const Hero = () => {
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[487px]">
-            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
-              Version 2.0 is Here
-            </div>
+            <div className="tag">Version 2.0 is Here</div>
             <h1 className="text-5xl md:text-7xl font-bold  tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">
               Pathway to Productivity
             </h1>
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,41 @@
+import Image from "next/image";
+import ProductIamge from "@/assets/product-image.png";
+import PyramindImage from "@/assets/pyramid.png";
+import TubeImage from "@/assets/tube.png";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-[#fff] to-[#d2dcff] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost Your Productivity</div>
+          </div>
+          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] bg-clip-text text-transparent mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010d3e] mt-5">
+            Effortlessly turn your ideas into a fully functional, responsive,
+            SaaS website in just minutes with this template.
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={ProductIamge} alt="Product Showcase" className="mt-10" />
+          <Image
+            src={PyramindImage}
+            alt="Pyramid"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+          />
+          <Image
+            src={TubeImage}
+            alt="Tube"
+            height={248}
+            className="hidden md:block absolute -left-36 bottom-24"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
